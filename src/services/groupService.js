31e5d1bbf87ef7e7d22a1b0078b1f3cefe2f9320
alok <alok.p@chapter247.com>
2024-09-16 17:43:49 +0530

    save

--- src/services/groupService.js
+++ src/services/groupService.js
@@ -15,22 +15,31 @@ export const saveGroups = async (data) => {
         for (let i = 0; i < members.length; i += data.numberOfGroup) {
             groups.push(members.slice(i, i + data.numberOfGroup));
         }
+        let transformedGroup = [];
+        groups.forEach(async (gp, groupIndex) => {
+            /**create group with name and save into db */
+            const groupresult = await groupModel.create({
+                userId,
+                name: `${name}_group_${groupIndex + 1}`,
+            });
+            console.log('groupresult--', groupresult);
 
-        // format members
-        // {
-        //     userId,
-        //     memberId,
-        //     groupId
-        // }
-        console.log('--gr', groups);
+            // Iterate over each item in the group
+            gp.forEach(async (item) => {
+                await groupMembers.create({
+                    _id: item._id,
+                    memeberId: item._id, // Hardcoded for now, can be dynamic
+                    creatorId: userId,           // Hardcoded for now, can be dynamic
+                    groupId: groupresult._id          // Hardcoded for now, can be dynamic
+                })
 
-        return
+            });
+        })
+
+        // return transformedGroup
         // create group by event data
-        await groupModel.create({
-            userId,
-            name,
-        });
-        await groupMembers.insertMany(groups);
+
+        // await groupMembers.insertMany(groups);
         return messages.itemAddedSuccess;
     }
     catch (error) {
